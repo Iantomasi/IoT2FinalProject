fb2f3478beee09729a050666004d98ecdac9c89f
Giuliano10 <juventinogiuliano@gmail.com>
2022-12-06 15:20:54 -0500

    Schema update

--- Schemas.py
+++ Schemas.py
@@ -1,4 +1,4 @@
 from marshmallow import Schema, fields, validate
 
 class UltraSonicSensorSchema(Schema):
-    temperature = fields.Number(required=True)
+    threadMeasurements = fields.Number(required=True)
