f34d6f66e05a90de28892e18335f155202ba9988
alexei timbro <alexei.timbro@gmail.com>
2022-12-07 00:37:56 -0500

    Emulator almost done. Prompts every 5 second but the measurement doesnt increase

---
+++ ArduinoEmulator.py
@@ -0,0 +1,28 @@
+import pymongo
+import datetime as dt
+from time import sleep
+
+
+
+client = pymongo.MongoClient("mongodb+srv://GMA:GMASeasonbaby123@iot2project.la8dvua.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+number = 2
+isSensorOn = True
+
+if 'measurement' not in db.list_collection_names():
+    db.create_collection("measurement",
+                         timeseries={'timeField': 'timestamp', 'metaField': 'sensorId', 'granularity': 'minutes'})
+
+
+if __name__ == "__main__":
+    while isSensorOn:
+        measurement = 0
+        sensorID = 7
+        number = 2
+        db.measurement.insert_one({
+            'timestamp': dt.datetime.now(),
+            'sensorId': sensorID,
+            'measurement': measurement
+        })
+        sleep(5)
