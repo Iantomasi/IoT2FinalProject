f757e704792148a59f585d1d0a40fe3e794873aa
Giuliano10 <juventinogiuliano@gmail.com>
2022-12-15 15:57:09 -0500

    Updating pipeline naming conventions

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def get_all_threadMeasurements(sensorId, measurement):
                     },
                     '_id': '$sensorId',
                     'measurement': '$measurement',
-                    'measurementCount': {
+                    'measurementMatch': {
                         '$count': {}
                     },
                     'measurement': {
